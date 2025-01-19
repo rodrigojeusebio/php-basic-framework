149bb92b952ca8a6dd0a9e49ee4bb573ed8fe99e
Rodrigo Eusébio <rodrigojeusebio@gmail.com>
2025-01-19 14:26:31 +0000

    Improve read me

--- App/Resources/Views/default_pages/404.php
+++ App/Resources/Views/default_pages/404.php
@@ -64,7 +64,7 @@
         <p class="mt-4 text-muted-foreground">Oops,
             the page you are looking for could not be found.</p>
         <div class="mt-6">
-            <a class="inline-flex items-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow-sm transition-colors hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
+            <a class="inline-flex items-center rounded-md bg-slate-800 text-white px-4 py-2 text-sm font-medium text-primary-foreground shadow-sm transition-colors hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
                 href="/">Go to Homepage </a>
         </div>
     </div>
--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # php-basic-framework
 
-Spin up
+### Spin up
+
 ```bash
 php -S localhost:8000
 ```
+
+### 404 - Not Found
+[404 - Not Found](./images/404.png)
+
+### Exception Handler during development
+[Exception Handler](./images/ExceptionViewer.png)
+
+### Logger
+[Logger](./images/Logger.png)
