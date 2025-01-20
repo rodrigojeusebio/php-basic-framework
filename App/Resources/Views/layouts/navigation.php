10b3da09685650869b22c05cbe4edb26d7af1929
Rodrigo Eusébio <rodrigojeusebio@gmail.com>
2025-01-20 19:01:43 +0000

    ADD LINK to home page

--- App/Resources/Views/layouts/navigation.php
+++ App/Resources/Views/layouts/navigation.php
@@ -4,6 +4,8 @@ use Core\Config;
 ?>
 <header class="bg-slate-800 text-white py-3 px-6 shadow-md">
     <div class="container mx-auto flex items-center justify-between">
-        <h1 class="text-2xl font-bold"><?= Config::get('app_name', 'PHP basic framework') ?></h1>
+        <a href="/">
+            <h1 class="text-2xl font-bold"><?= Config::get('app_name', 'PHP basic framework') ?></h1>
+        </a>
     </div>
 </header>
