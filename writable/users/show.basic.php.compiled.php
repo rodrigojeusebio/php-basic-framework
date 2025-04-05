36c48d7082308cd344580eb8615e1ea6ff4a4033
Rodrigo Eusébio <rodrigojeusebio@gmail.com>
2025-04-05 19:41:42 +0100

    remove writable

--- writable/users/show.basic.php.compiled.php
+++
@@ -1,7 +0,0 @@
-<h1 class="text-lg font-bold">Hello <?  echo htmlspecialchars($user_name);  ?> (<?  echo htmlspecialchars($user_id);  ?>)</h1>
-
-<h3 class="text-lg">Delete user</h3>
-<form action="/users/<?  echo htmlspecialchars($user_id);  ?>" method="post">
-<input type="hidden" name="_method" value="delete">
-    <button class="btn border py-2 px-4 rounded-md text-white bg-red-500 hover:bg-red-600">Submit</button>
-</form>
